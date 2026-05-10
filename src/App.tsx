c04c8d00a8e1f1881472e118c7ae447d39044788
EJay <carnocentaurus@gmail.com>
2026-05-10 17:02:00 +0800

    libraries and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
 
 function MarkdownLiveEditor() {
+    const [plaintext, setPlaintext] = useState('');
+
+    function getCleanedHTML(text: string): string {
+        const cleanedText = text.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, "");
+        const rawHTML = marked.parse(cleanedText) as string;
+        
+        return DOMPurify.sanitize(rawHTML);
+    }
+
     return(
         <>
             <p>MarkdownLiveEditor</p>
 
             <main>
-                
+                <div>
+                      <textarea
+                          value={plaintext}
+                          onChange={(event) => setPlaintext(event.target.value)}
+                          placeholder='Input Text'
+                      ></textarea>
+                </div>
+
+                <div>
+                      <div
+                          dangerouslySetInnerHTML={{ __html: getCleanedHTML(plaintext) }}
+                      ></div>
+                      <button onClick={window.print}>Print PDF</button>
+                </div>
             </main>
         </>
     );
